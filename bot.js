b28d18b3ca7869af8eb70201af991a66bad6671e
mohammedezz5 <45146103+mohammedezz5@users.noreply.github.com>
2018-11-21 15:35:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "11";
+var prefix = "12";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
